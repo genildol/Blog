286a81e9ecf7fb1312b28da2af733ea61121947f
Genildo Lopes <jgmicroshs@gmail.com>
2025-04-13 21:56:29 -0300

    creating routes and controllers

---
+++ blog-backend/src/controllers/auth.ts
@@ -0,0 +1,5 @@
+import { RequestHandler } from "express"
+
+export const signup: RequestHandler = async (req, res) => {
+
+}
---
+++ blog-backend/src/controllers/main.ts
@@ -0,0 +1,13 @@
+import { RequestHandler } from "express";
+
+export const getAllPosts: RequestHandler = async (req, res) => {
+
+}
+
+export const getPosts: RequestHandler = async (req, res) => {
+
+}
+
+export const getRelatedPosts: RequestHandler = async (req, res) => {
+
+}
