29517695daf1a04d2270f8cda00eb564cad11962
Genildo Lopes <jgmicroshs@gmail.com>
2025-04-15 22:20:40 -0300

    Creating a new user

--- blog-backend/src/controllers/auth.ts
+++ blog-backend/src/controllers/auth.ts
@@ -1,5 +1,33 @@
 import { RequestHandler } from "express"
+import { z } from "zod";
+import { createUser } from "../services/user";
 
 export const signup: RequestHandler = async (req, res) => {
+  const schema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string()
+  });
+  const data = schema.safeParse(req.body);
+  if (!data.success) {
+    res.json({ error: data.error?.flatten().fieldErrors });
+    return
+  }
 
+  const newUser = await createUser(data.data);
+  if (!newUser) {
+    res.json({ error: "Erro ao criar usuário" });
+    return;
+  }
+  const token = '123'; //TODO: gerar token
+
+  res.status(201).json({
+    user: {
+      id: newUser.di,
+      name: newUser.name,
+      email: newUser.email,
+    },
+    token
+  });
 }
+
